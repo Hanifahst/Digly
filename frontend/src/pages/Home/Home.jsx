e6852cc2637d780186cedbb4372b49d3a4906132
Syifa Shahab <shahabsyifa30u@gmail.com>
2026-06-16 20:21:49 +0700

    feat: configure Tailwind CSS

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,19 @@
+import Navbar from "../../components/layout/Navbar/Navbar";
+import Footer from "../../components/layout/Footer/Footer";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Navbar />
+
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">Welcome to Digly 📚</h1>
+        <p className="mt-2">Digital Library for everyone.</p>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
